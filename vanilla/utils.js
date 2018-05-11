708558a7097f46c7a445fbe1ffa0838b5561b805
tahara-kazuki <tahara-kazuki@dmm.com>
2018-05-11 13:55:46 +0900

    fix
    vanilla/utils.js

--- vanilla/utils.js
+++ vanilla/utils.js
@@ -1,8 +1,17 @@
 /**
  * email validation
+ * mailアドレスのバリデーション関数
+ * -> ○○○@○○○ なら通過
  * @param email
  *
  */
 
 export const emailFormatChecker = email => /^[a-zA-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?)*$/.test(email);
 
+/**
+ * pick url
+ * textの中にあるurl形式の箇所をpickUpしてaタグで囲む関数
+ * @param text
+ */
+
+export const pickUrl = text => (text.replace(/((https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+))/gi,"<a href='$1' target='_blank'>$1</a>"));
