6aef045bfd6af64becf484e712e3803b5e4e6abf
tahara-kazuki <tahara-kazuki@dmm.com>
2018-05-14 12:00:06 +0900

    fix
    vanilla/utils.js

--- vanilla/utils.js
+++ vanilla/utils.js
@@ -1,7 +1,7 @@
 /**
  * email validation
  * mailアドレスのバリデーション関数
- * -> ○○○@○○○ なら通過
+ * -> ○○○@○○○ の形式なら通過
  * @param email
  *
  */
@@ -11,7 +11,33 @@ export const emailFormatChecker = email => /^[a-zA-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[
 /**
  * pick url
  * textの中にあるurl形式の箇所をpickUpしてaタグで囲む関数
+ * ex: 'テストテストテストhttps://www.hoge.hogeテストテストテスト'
+ * -> 'テストテストテスト<a href="https://www.hoge.hoge" target='_blank'>https://www.hoge.hoge</a>テストテストテスト'
  * @param text
  */
 
 export const pickUrl = text => (text.replace(/((https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+))/gi,"<a href='$1' target='_blank'>$1</a>"));
+
+/**
+ * check postal code
+ * 郵便番号形式のvalidation
+ * @param postalCode
+ */
+
+export const checkPostalCode = postalCode => /[0-9]{3}-?[0-9]{4}/.test(postalcode);
+
+/**
+ * check phoneNumber
+ * 電話番号のvalidation
+ * @param phoneNumber
+ */
+
+export const checkPhoneNumber = phoneNumber => /[0-9-]{6,9}$|[0-9-]{12}/.test(phoneNumber);
+
+/**
+ * check mobilePhoneNumber
+ * 携帯電話番号のみのvalidation
+ * @param mobilePhoneNumber
+ */
+
+export const checkMobilePhoneNumber = mobilePhoneNumber => /\d{3}-\d{4}-\d{4}$|\d{11}/.test(mobilePhoneNumber);
